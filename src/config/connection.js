cc0550fce182e5dc254e9d0cbf4b4417b7360d74
isahbella007 <okorofth@gmail.com>
2023-05-11 21:16:13 -0700

    editing mysql connection

--- src/config/connection.js
+++ src/config/connection.js
@@ -4,7 +4,14 @@ const env = require("dotenv");
 env.config({ path: "././.env" });
 
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
-const db_connection = mysql.createConnection(urlDB);
+const db_connection = mysql.createPool({
+  connectionLimit: 10, 
+  host: process.env.MYSQLHOST, 
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE, 
+  port: process.env.MYSQLHOST
+})
 db_connection.connect((err) => {
   if (err) {
     console.log(err);
